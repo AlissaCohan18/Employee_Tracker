318e920be08d61c2d101a2ae6ccd4ccceb5ea42c
Alissa Cohan <alissacohan18@gmail.com>
2023-01-31 17:03:12 -0500

    remaining add and updatef functions completedd

--- index.js
+++ index.js
@@ -98,7 +98,7 @@ const viewAllEmployees = () => {
 };
 
 //prompted to enter the name of the department and that department is added to the database
-const  addDepartment= () => {
+const addDepartment = () => {
   return inquirer
     .prompt([
       {
@@ -108,23 +108,121 @@ const  addDepartment= () => {
       },
     ])
     .then((input) => {
-      const params = input.name
+      const params = input.name;
       const sql = `INSERT INTO department (name) VALUES (?)`;
       db.query(sql, params, (err, res) => {
-          if (err) throw err;
-          return input;
+        if (err) throw err;
+        return input;
+      });
+    })
+    .then(() => {
+      promptUser();
     });
-  })
-  .then(() => {
-    promptUser()
-  })
 };
 
 // // prompted to enter the name, salary, and department for the role and that role is added to the database
-// const  addRole
+const addRole = () => {
+  return inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "title",
+        message: "What role would you like to add?",
+      },
+      {
+        type: "input",
+        name: "salary",
+        message: "What's the salary?",
+      },
+      {
+        type: "input",
+        name: "department_id",
+        message: "What's the department's ID?",
+      },
+    ])
+    .then((input) => {
+      const params = [input.title, input.salary, input.department_id];
+      const sql = `INSERT INTO roles (title, salary, department_id) VALUES (?,?,?)`;
+      db.query(sql, params, (err, res) => {
+        if (err) throw err;
+        return input;
+      });
+    })
+    .then(() => {
+      promptUser();
+    });
+};
 
 // // prompted to enter the employee’s first name, last name, role, and manager, and that employee is added to the database
-// const  addEmployee
+const addEmployee = () => {
+  return inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "first_name",
+        message: "What's the employee's first name?",
+      },
+      {
+        type: "input",
+        name: "last_name",
+        message: "What's the employee's last name?",
+      },
+      {
+        type: "input",
+        name: "role_id",
+        message: "What is their role ID?",
+      },
+      {
+        type: "input",
+        name: "manager_id",
+        message: "What is their manager's ID?",
+      },
+    ])
+    .then((input) => {
+      const params = [
+        input.first_name,
+        input.last_name,
+        input.role_id,
+        input.manager_id,
+      ];
+      const sql = `INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?,?,?,?)`;
+      db.query(sql, params, (err, res) => {
+        if (err) throw err;
+        return input;
+      });
+    })
+    .then(() => {
+      promptUser();
+    });
+};
 
-// // prompted to select an employee to update and their new role and this information is updated in the database
-// const  updateEmployee
+// prompted to select an employee to update and their new role and this information is updated in the database
+const updateEmployee = () => {
+  return inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "id",
+        message: "What's the ID of the employee you want to update?",
+      },
+      {
+        type: "input",
+        name: "role_id",
+        message: "What will be their new role ID?",
+      },
+    ])
+    .then((input) => {
+      const params = [
+        input.id,
+        input.role_id,
+      ];
+      const sql = `UPDATE employee SET role_id = ? WHERE id = ?`;
+      db.query(sql, params, (err, res) => {
+        if (err) throw err;
+        return input;
+      });
+    })
+    .then(() => {
+      promptUser();
+    });
+};
